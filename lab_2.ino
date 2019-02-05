f60ac7209fb353e51a0d826939c9f2fb8d079981
weji9884 <35515301+weji9884@users.noreply.github.com>
2019-02-04 20:10:56 -0700

    Add files via upload
    lab_2bb is modified over Arduino example file "simple".

---
+++ lab_2.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  // declare inouts and outputs
+  pinMode(13, OUTPUT);// declares pin 13 as outout pin 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
